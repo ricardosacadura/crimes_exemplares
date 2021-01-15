f870e74538a475fd684332593c3f71e0d4eb48b8
marianassilvas <crania.mariana@gmail.com>
2021-01-15 22:06:31 +0000

    ajustes

--- js/conto.js
+++ js/conto.js
@@ -3,19 +3,20 @@ function saidiabo(event) {
 
 
   //lê o y (- para ir para o scroll ser para baixo, o numero é a velocidade a que acontece,quanto menor mais lento)
-  opacidade += event.deltaY * -0.0004;
+  opacidade += event.deltaY * -0.0001;
 
-  // Restrict scale
+  // restringir a opacidade máximo 1 e mínimo 0
   opacidade = Math.min(Math.max(0, opacidade), 1);
 
-  // Apply scale transform
+  // aplica a opacidade
   ret.style.opacity = opacidade;
 
+ //desaparece da página
   if (opacidade == 0){
     ret.style.zIndex = -1;
   }
 }
-
+//quando scrolla (onwheel)
 let opacidade = 1;
 let ret = document.querySelector('.retangulo');
 ret.onwheel = saidiabo;
